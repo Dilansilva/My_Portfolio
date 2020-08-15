e28d9d77c4a6f6a9a2c158c983d209eb19b0d9f8
Dilan Silva <sdilansilva@gmail.com>
2020-08-15 23:14:42 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('', (req, res) => {
+    res.render('inde') 
+  })
+
+  
+
+      app.listen(port, () => {
+        console.log('My Portfoli')
+      })
